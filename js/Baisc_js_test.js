c15542e000e7e2dad0dde98375875a6ebe096d65
admin <jeonyj1203@gmail.com>
2024-04-03 17:19:59 +0900

    5주차

--- js/Baisc_js_test.js
+++ js/Baisc_js_test.js
@@ -19,8 +19,3 @@ const e = '상수1 접근';
 //const f //Error?
 console.log(e);
 
-document.getElementById("search_btn").addEventListener('click',search_message);
-
-function search_message() {
-    alert("검색을 수행합니다!")
-}
