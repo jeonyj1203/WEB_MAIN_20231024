8c61534613ed0dd33ee00ec87c813cb08b9b267b
admin <jeonyj1203@gmail.com>
2024-05-22 17:15:49 +0900

    12주차

--- js/login.js
+++ js/login.js
@@ -16,6 +16,60 @@ const check_xss = (input) => {
     return sanitizedInput;
     };
 
+function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
+    const emailInput = document.getElementById('typeEmailX');
+    const idsave_check = document.getElementById('idSaveCheck');
+    let get_id = getCookie("id");
+    if(get_id) {
+        emailInput.value = get_id;
+        idsave_check.checked = true;
+    }
+    session_check(); 
+}
+
+function session_check() { //세션 검사
+    if (sessionStorage.getItem("Session_Storage_test")) {
+        alert("이미 로그인 되었습니다.");
+        location.href='../login/index_login.html'; // 로그인된 페이지로 이동
+    }
+}
+
+
+function encodeByAES256(key, data){
+    const cipher = CryptoJS.AES.encrypt(data, CryptoJS.enc.Utf8.parse(key), {
+        iv: CryptoJS.enc.Utf8.parse(""),
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return cipher.toString();
+    }
+
+    function decodeByAES256(key, data){
+        const cipher = CryptoJS.AES.decrypt(data, CryptoJS.enc.Utf8.parse(key), {
+            iv: CryptoJS.enc.Utf8.parse(""),
+            padding: CryptoJS.pad.Pkcs7,
+            mode: CryptoJS.mode.CBC
+        });
+    return cipher.toString(CryptoJS.enc.Utf8);
+    }
+
+    function encrypt_text(password){
+        const k = "key"; // 클라이언트 키
+        const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+        const b = password;
+        const eb = this.encodeByAES256(rk, b);
+        return eb;
+        console.log(eb);
+    }
+    function decrypt_text(){
+        const k = "key"; // 서버의 키
+        const rk = k.padEnd(32, " "); // AES256은 key 길이가 32
+        const eb = session_get();
+        const b = this.decodeByAES256(rk, eb);
+        console.log(b);
+    }
+        
+    
 
 const check_input = () => {
     const loginForm = document.getElementById('login_form');
@@ -85,7 +139,7 @@ const check_input = () => {
         { // 아이디 체크 x
             setCookie("id", emailValue.value, 0); //날짜를 0 - 쿠키 삭제
         }
-
+    session_set();
     loginForm.submit();
 };
 
@@ -108,16 +162,31 @@ function getCookie(name) {
         }
     }
     return ;
-    }
+}
 
-    function init(){ // 로그인 폼에 쿠키에서 가져온 아이디 입력
-        const emailInput = document.getElementById('typeEmailX');
-        const idsave_check = document.getElementById('idSaveCheck');
-        let get_id = getCookie("id");
-        if(get_id) {
-            emailInput.value = get_id;
-            idsave_check.checked = true;
-        }
-    }
 
+
+function session_set() { //세션 저장
+    let session_id = document.querySelector("#typeEmailX"); // DOM 트리에서 ID 검색
+    let session_pass = document.querySelector("#typePasswordX"); // DOM 트리에서 pass 검색
+    if (sessionStorage) {
+        let en_text = encrypt_text(session_pass.value);
+        sessionStorage.setItem("Session_Storage_id", session_id.value);
+        sessionStorage.setItem("Session_Storage_pass", en_text);
+    } else {
+        alert("로컬 스토리지 지원 x");
+    }
+    }
+    
+
+function session_get() { //세션 읽기
+    if (sessionStorage) {
+        return sessionStorage.getItem("Session_Storage_test");
+    } 
+    else {
+        alert("세션 스토리지 지원 x");
+    }
+}
+
+    
 document.getElementById("login_btn").addEventListener('click', check_input);
