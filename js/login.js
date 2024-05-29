f17ca30c92b984ba82657f8de7151ee294aaf17e
admin <jeonyj1203@gmail.com>
2024-05-29 16:55:13 +0900

    13주차

--- js/login.js
+++ js/login.js
@@ -188,5 +188,24 @@ function session_get() { //세션 읽기
     }
 }
 
+function session_del() {//세션 삭제
+    if (sessionStorage) {
+        sessionStorage.removeItem("Session_Storage_test");
+        alert('로그아웃 버튼 클릭 확인 : 세션 스토리지를 삭제합니다.');
+    } 
+    else {
+        alert("세션 스토리지 지원 x");
+    }
+    }
+
+function init_logined(){
+    if(sessionStorage){
+            decrypt_text(); // 복호화 함수
+    }
+    else{
+        alert("세션 스토리지 지원 x");
+    }
+}
+        
     
-document.getElementById("login_btn").addEventListener('click', check_input);
+document.getElementById('login_btn').addEventListener('click', check_input);
